8713976c3a4f4aed0e0294f86c3ace3c675d18a0
Umang Doshi <umangdoshi151@gmail.com>
2025-02-15 06:46:57 +0530

    Resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-// import Home from "./frontend/Home";
-import SnellenTest from "./frontend/SnellenTest/SnellenTest";
-// import LoadBalancer from "./frontend/Loader/Loader";
-=======
+
 import Home from "../src/frontend/Home/Home";
-// import SnellenTest from "./frontend/SnellenTest/SnellenTest";
+import SnellenTest from "./frontend/SnellenTest/SnellenTest";
 import Loader from "./frontend/Loader/Loader";
 import SnellenLoader from "./frontend/SnellLoader/SnellenLoader";
->>>>>>> a7d14816ceb46eb47b46c69f4ac556a751b8de9c
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<SnellenTest />} />
-          {/* <Route path="/" element={<LoadBalancer />} />
-          <Route path="/snellen-test" element={<SnellenTest />} /> */}
-=======
           <Route path="/" element={<Loader />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/snellLoader" element={<SnellenLoader/>} />
-          {/* <Route path="/snellen-test" element={<SnellenTest />} /> */}
->>>>>>> a7d14816ceb46eb47b46c69f4ac556a751b8de9c
+          <Route path="/snellLoader" element={<SnellenLoader />} />
+          <Route path="/snellen-test" element={<SnellenTest />} />
+
         </Routes>
       </div>
     </Router>
@@ -32,22 +23,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import Home from "./frontend/Home";
-// import SnellenTest from "./frontend/SnellenTest/SnellenTest";
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//        {/* <Home /> */}
-//        <SnellenTest />
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
